1d6dfad75d04b39bfc7aca1591259246af29fa1d
Ronin shin <ronin.shin@cheilpengtai.com>
2019-08-23 18:14:00 +0900

    insert firebase-push

---
+++ firebase-init.js
@@ -0,0 +1,38 @@
+
+
+        // Your web app's Firebase configuration
+        var firebaseConfig = {
+            apiKey: "AIzaSyBx1U-MM_GNzHuCOTFSGZP0_hwAIVGvn-4",
+            authDomain: "ronin-3a009.firebaseapp.com",
+            databaseURL: "https://ronin-3a009.firebaseio.com",
+            projectId: "ronin-3a009",
+            storageBucket: "ronin-3a009.appspot.com",
+            messagingSenderId: "591023826852",
+            appId: "1:591023826852:web:0314ac15b6622666"
+        };
+        // Initialize Firebase
+        firebase.initializeApp(firebaseConfig);
+
+        const messaging = firebase.messaging();
+        messaging.setBackgroundMessageHandler(function(payload){
+
+            const title = "Hello World";
+            const options = {
+                    body: payload.data.status
+            };
+
+            return self.registration.showNotification(title,options);
+        });
+
+        //token값 알아내기
+        messaging.requestPermission()
+          .then(function(){
+              console.log("Have permission");
+              return messaging.getToken();
+          })
+          .then(function(token){
+              alert(token);
+          })
+          .catch(function(arr){
+              alert("Error Occured");
+          });
---
+++ firebase-messaging-sw.js
@@ -0,0 +1,15 @@
+importScripts('https://www.gstatic.com/firebasejs/6.4.1/firebase-app.js');
+importScripts('https://www.gstatic.com/firebasejs/6.4.1/firebase-messaging.js');
+
+        // Your web app's Firebase configuration
+        var firebaseConfig = {
+            apiKey: "AIzaSyBx1U-MM_GNzHuCOTFSGZP0_hwAIVGvn-4",
+            authDomain: "ronin-3a009.firebaseapp.com",
+            databaseURL: "https://ronin-3a009.firebaseio.com",
+            projectId: "ronin-3a009",
+            storageBucket: "ronin-3a009.appspot.com",
+            messagingSenderId: "591023826852",
+            appId: "1:591023826852:web:0314ac15b6622666"
+        };
+        // Initialize Firebase
+        firebase.initializeApp(firebaseConfig);
